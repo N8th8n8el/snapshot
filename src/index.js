959a11e492cc609008842d07deb3a009ceaeef52
Nathanael Ruf <nathanael97@protonmail.com>
2020-10-19 12:35:24 +0200

    log: true

--- src/index.js
+++ src/index.js
@@ -201,7 +201,7 @@ function registerCypressSnapshot () {
       snapshots.__version = Cypress.version
       const s = JSON.stringify(snapshots, null, 2)
       const str = `module.exports = ${s}\n`
-      cy.then(() => cy.writeFile(snapshotFileName, str, 'utf-8', { log: false }));
+      cy.then(() => cy.writeFile(snapshotFileName, str, 'utf-8', { log: true }))
     }
   })
 
